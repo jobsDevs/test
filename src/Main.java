7febfdacf4bad185372f2d4c9a54a72230395b64
Alexander <89515014507@mail.ru>
2022-10-28 20:05:46 +0600

    Изменил переменную в классе main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        // наш первый репозиторий на гите
+        int num = 100;
+        System.out.println(num);
+
+        String line = "Draft for test Git";
+
+
+    }
+}
