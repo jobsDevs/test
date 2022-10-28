bde59e26f687d7116a91b492bd67556d701797b1
Alexander <89515014507@mail.ru>
2022-10-28 19:52:55 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        // наш первый репозиторий на гите
+        int num = 10;
+        System.out.println(num);
+
+
+    }
+}
